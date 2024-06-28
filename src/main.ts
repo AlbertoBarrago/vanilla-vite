62290186c6bb2f3643278a0114ab0495e873bc67
Alberto Barrago <albertobarrago@gmail.com>
2024-06-28 15:47:34 +0200

    Added base pagination with module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,31 @@ import './style.css'
 document.addEventListener('DOMContentLoaded', () => {
     const app = document.getElementById('app')!;
 
+    const navigation = () => `
+      <nav>
+          <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+    `
+
+    const footer = () => `
+        <footer>
+            <p>Copyright 2021</p>
+        </footer>
+     `
+
+    const template = (content: string) => `
+    ${navigation()} ${content} ${footer()}`;
+
     const routes: {
         [key: string]: string;
     } = {
-        '/': '<h1>Home Page</h1>',
-        '/about': '<h1>About Page</h1>',
-        '/contact': '<h1>Contact Page</h1>',
+        '/': `${template('<h1>Home Page</h1>')}`,
+        '/about': `${template('<h1>About Page</h1>')}`,
+        '/contact': `${template('<h1>Contact Page</h1>')}`,
     };
 
     const render = (path: string) => {
@@ -16,12 +35,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.onpopstate = () => render(window.location.pathname);
 
-    document.addEventListener('click', (e: any) => {
-        if (e.target?.tagName === 'A' && e.target?.href.startsWith(window.location.origin)) {
+    document.addEventListener('click', (e: MouseEvent) => {
+        const target = e.target as HTMLAnchorElement
+        if (target.tagName === 'A' && target.href.startsWith(window.location.origin)) {
             e.preventDefault();
-            const path = e.target.getAttribute('href');
-            window.history.pushState({}, path, window.location.origin + path);
-            render(path);
+            const path = target.getAttribute('href');
+            window.history.pushState({}, path!, window.location.origin + path);
+            render(path!);
         }
     });
 
